00fc3395300014444874f7863f654e19e53b1475
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-08-11 16:54:11 +0000

    O clique no botão favorito no menu está provocando um problema. Ao clica

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -175,12 +175,12 @@ export default function Home() {
       <div 
         onMouseLeave={handleMouseLeaveMenu}
         className={cn(
-            "fixed top-0 left-0 right-0 z-50 transition-transform duration-700 ease-in-out",
+            "fixed top-0 left-0 right-0 z-50 transition-transform duration-700 ease-in-out bg-black/90 backdrop-blur-sm",
             isMenuOpen ? "translate-y-0" : "-translate-y-full",
             "will-change-transform"
         )}
       >
-        <div className="bg-black/90 backdrop-blur-sm pt-6 pb-6">
+        <div className="pt-6 pb-6">
            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center">
                 <h1 className="text-3xl font-bold tracking-wider text-white">PORTFÓLIO - CIRCUITO CARIOCA DE FEIRAS ORGÂNICAS</h1>
                 <p className="mt-4 text-base text-gray-300">Aqui você encontra todas as artes produzidas ao longo de mais de uma década, com apoio da organização Essência Vital, para a comunicação, propaganda e marketing de suporte às feiras orgânicas do Circuito Carioca e suas famílias de agricultores.</p>
