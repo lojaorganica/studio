3cf7caa274ae38f628f9b7560cf82df654e7f640
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-09-20 21:17:31 +0000

    No menu lateral, torne as setinhas brancas de abrir e fechar o menu acor

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -34,7 +34,7 @@ const AccordionTrigger = React.forwardRef<
       {...props}
     >
       <div className="flex-1 text-left">{children}</div>
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      <ChevronDown className="h-5 w-5 shrink-0 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
