948663a012e864c6786492be998e3bf237923ac9
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-10-05 20:45:01 +0000

    Agora s imagens de story voltaram a desaparecer.

--- src/components/lightbox.tsx
+++ src/components/lightbox.tsx
@@ -37,9 +37,9 @@ export function Lightbox({ item, onClose, onNext, onPrev }: LightboxProps) {
               {/* Media container for Story */}
               <div className={cn(
                 "relative flex items-center justify-center w-full max-h-[60vh] md:max-h-[85vh] transition-transform duration-300",
-                 !isActuallyStoryFormat ? "md:scale-105 md:w-[55%]" : "md:w-auto"
+                 !isActuallyStoryFormat ? "md:scale-105 md:w-[55%]" : ""
               )}>
-                <div className={cn("relative h-full w-full", isActuallyStoryFormat ? "aspect-[9/16] md:max-w-md lg:max-w-lg" : "aspect-[3/4]")}>
+                <div className={cn("relative h-full w-full", isActuallyStoryFormat ? "aspect-[9/16] md:w-80 lg:w-96" : "aspect-[3/4]")}>
                    {item.type === 'video' ? (
                     <video
                       src={item.src}
