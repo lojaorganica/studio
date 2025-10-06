f86678173e74c4e3990beaa07aeb68e95d94c0cf
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-10-06 00:38:16 +0000

    Não está funcionando. Não seria melhor desistir e voltar com o App sem a

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,9 @@
 'use server';
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI} from '@genkit-ai/google-genai';
 import {googleCloud} from '@genkit-ai/google-cloud';
 
 export const ai = genkit({
-  plugins: [googleAI(), googleCloud],
+  plugins: [googleAI(), googleCloud()],
   model: 'googleai/gemini-1.5-flash',
 });
