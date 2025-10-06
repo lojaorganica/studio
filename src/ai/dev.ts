282092a6a6216ea43429b29f48f75b7ba003db33
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-10-06 00:39:58 +0000

    Segue com erro

--- src/ai/dev.ts
+++
@@ -1,3 +0,0 @@
-// Flows will be imported for their side effects in this file.
-import './flows/assistant-flow';
-import './flows/text-to-speech-flow';
--- src/ai/genkit.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/google-genai';
-import {googleCloud} from '@genkit-ai/google-cloud';
-
-export const ai = genkit({
-  plugins: [googleAI(), googleCloud()],
-  model: 'googleai/gemini-1.5-flash',
-});
