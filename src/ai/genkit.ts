665d393ba4c097954693f3d3ccc07420d0e54758
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-10-06 00:58:00 +0000

    Deu erro

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,9 @@
-'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import {googleCloud} from '@genkit-ai/google-cloud';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI(), googleCloud],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
 });
