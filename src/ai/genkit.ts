ba508be1243a935cff0520786f1279e7003352df
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-10-06 00:31:33 +0000

    Segue dando erro e sem aparecer meu app

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,6 +4,6 @@ import {googleAI} from '@genkit-ai/googleai';
 import {googleCloud} from '@genkit-ai/google-cloud';
 
 export const ai = genkit({
-  plugins: [googleAI, googleCloud],
+  plugins: [googleAI(), googleCloud],
   model: 'googleai/gemini-1.5-flash',
 });
